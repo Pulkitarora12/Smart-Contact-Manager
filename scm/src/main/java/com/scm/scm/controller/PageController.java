da17ea30e48370661fe52c0f700b45685fde37df
Pulkit Arora <pulkitaora0714@gmail.com>
2025-07-18 23:46:02 +0530

    day-1

---
+++ scm/src/main/java/com/scm/scm/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.scm.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("name", "Pulkit Arora");
+        model.addAttribute("Github", "https://github.com/Pulkitarora12");
+        return "home"; // This will render home.html from templates
+    }
+}
+
